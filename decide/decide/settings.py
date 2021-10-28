75277fd4f38472726a864ad2b132cc4c0dad0eaa
jagalindo <jagalindo@us.es>
2021-10-28 08:50:34 +0200

    Adding heroku support

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import os
-
 import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -70,7 +70,7 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://gh-actions-travis'
+BASEURL = 'https://gh-actions-travis'
 APIS = {
     'authentication': BASEURL,
     'base': BASEURL,
